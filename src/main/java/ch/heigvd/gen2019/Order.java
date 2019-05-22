dbaa7e22d2ae719bc7c03951e3d5a4365527969b
Jorge <jorge.fulgencioesteves@heig-vd.ch>
2019-05-22 13:30:02 +0200

    Deadcode: removed it in Order

--- src/main/java/ch/heigvd/gen2019/Order.java
+++ src/main/java/ch/heigvd/gen2019/Order.java
@@ -10,19 +10,6 @@ public class Order {
     public Order(int id) {
         this.id = id;
     }
-
-    public int getOrderId() {
-        return id;
-    }
-
-    public int getProductsCount() {
-        return products.size();
-    }
-
-    public Product getProduct(int j) {
-        return products.get(j);
-    }
-
     public void AddProduct(Product product) {
         products.add(product);
     }

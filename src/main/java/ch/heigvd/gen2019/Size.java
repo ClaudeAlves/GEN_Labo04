6048e4f53fa90a811bffdc08ac1b344a524c3a1b
Jorge <jorge.fulgencioesteves@heig-vd.ch>
2019-05-22 13:13:21 +0200

    Replace Conditional with Enum on size field in product

---
+++ src/main/java/ch/heigvd/gen2019/Size.java
@@ -0,0 +1,10 @@
+package ch.heigvd.gen2019;
+
+public enum Size {
+    XS,
+    S,
+    M,
+    L,
+    XL,
+    SIZE_NOT_APPLICABLE;
+}
--- src/test/java/ch/heivd/gen2019/OrdersWriterTest.java
+++ src/test/java/ch/heivd/gen2019/OrdersWriterTest.java
@@ -36,7 +36,7 @@ public class OrdersWriterTest {
 
     @Test
     public void OneOrderWithOneProduct() {
-        order111.AddProduct(new Product("Shirt", Color.blue, 3, 2.99, "TWD"));
+        order111.AddProduct(new Product("Shirt", Color.blue, Size.M, 2.99, "TWD"));
 
         String order111Json = JsonOrder111WithProduct("{\"code\": \"Shirt\", \"color\": \"blue\", \"size\": \"M\", \"price\": 2.99, \"currency\": \"TWD\"}");
         assertEquals("{\"orders\": [" + order111Json + "]}", orders.getContents());
@@ -44,7 +44,7 @@ public class OrdersWriterTest {
 
     @Test
     public void OneOrderWithOneProductNoSize() {
-        order111.AddProduct(new Product("Pot", Color.red, -1, 16.50, "SGD"));
+        order111.AddProduct(new Product("Pot", Color.red, Size.SIZE_NOT_APPLICABLE, 16.50, "SGD"));
 
         String order111Json = JsonOrder111WithProduct("{\"code\": \"Pot\", \"color\": \"red\", \"price\": 16.5, \"currency\": \"SGD\"}");
         assertEquals("{\"orders\": [" + order111Json + "]}", orders.getContents());

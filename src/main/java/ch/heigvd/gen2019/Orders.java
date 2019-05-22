fa0154a09122a1ec8f8bc9dce7dad9d4c535781e
Jorge <jorge.fulgencioesteves@heig-vd.ch>
2019-05-22 12:49:12 +0200

    Inline temp variable order in Orders-getContents() adn switched to foreach loop

--- src/main/java/ch/heigvd/gen2019/Orders.java
+++ src/main/java/ch/heigvd/gen2019/Orders.java
@@ -17,8 +17,7 @@ public class Orders {
     public String getContents() {
         StringBuffer sb = new StringBuffer("{\"orders\": [");
 
-        for (int i = 0; i < orders.size(); i++) {
-            Order order = getOrder(i);
+        for (Order order : orders) {
             order.getContent(sb);
         }
 
